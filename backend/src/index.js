457c1b4052058a236725321b798d2db3f7033128
Vivek Velugoti <vivekvelugoti070705@gmail.com>
2026-02-22 19:47:17 +0530

    Set Node version to 20 for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,25 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:5137",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://your-frontend-url.onrender.com"
+        : "http://localhost:5137",
     credentials: true,
   })
 );
@@ -31,12 +33,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server running on PORT: " + PORT);
   connectDB();
 });
