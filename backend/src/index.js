db2f1aeab2fe106b368d8addecbc705c668ee6fb
Vivek Velugoti <vivekvelugoti070705@gmail.com>
2026-02-22 20:16:47 +0530

    Set Node version to 20 for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,44 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import path from "path";
-
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js'
+import dotenv from 'dotenv';
+import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import {app,server} from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
-
-const PORT = process.env.PORT || 5001;
+app.use(express.json({ limit: '200kb' }));
+app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
+const PORT=process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use((err, req, res, next) => {
+  if (err.type === 'entity.too.large') {
+    console.log("file too large");
+    return res.status(413).json({ message: 'Payload too large. Max size is 200KB.' });
+  }
 
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://your-frontend-url.onrender.com"
-        : "http://localhost:5137",
-    credentials: true,
-  })
-);
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+  console.error("Unhandled error:", err);
+  res.status(500).json({ message: 'Internal server error' });
+});
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
-server.listen(PORT, () => {
-  console.log("Server running on PORT: " + PORT);
-  connectDB();
-});
+server.listen(PORT,()=>{
+    console.log("Server is running on port :"+PORT);
+    connectDB();
+})
