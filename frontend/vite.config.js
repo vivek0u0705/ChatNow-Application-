ba282ac8e27350ff9cf2b31ea725969a884b23b9
Vivek Velugoti <vivekvelugoti070705@gmail.com>
2026-02-23 13:01:41 +0530

    Set Node version to 20 for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5137,
-    strictPort: true,
-  },
+  plugins: [react(),
+    tailwindcss(),
+  ],
 })
